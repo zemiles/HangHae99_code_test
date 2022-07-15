31a6bbddf2a35086f4990980f15e1f3f9efa78ea
keembogeul <318qhtmf@naver.com>
2022-07-15 20:49:02 +0900

    Solution01 수정

--- src/Solution01.java
+++ src/Solution01.java
@@ -1,8 +1,8 @@
 public class Solution01 {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 24;
+    public String solution(int a, int b) {
+        // 1월 1일 = 금요일
         String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+
         int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int sum = 0;
 
@@ -12,6 +12,17 @@ public class Solution01 {
         sum += b - 1;
 
         String answer = day[sum % 7];
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution01 sol = new Solution01();
+
+        int a = 5;
+        int b = 24;
+
+        String answer = sol.solution(a, b);
+        
         System.out.println(answer);
     }
 }
